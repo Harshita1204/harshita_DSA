9de3243534b6b5cab1849fbd4d61d30ca58b2dce
Harshita1204 <harshitak1204@gmail.com>
2026-02-08 19:51:30 +0530

    pointer1.cpp

--- Luv/pointer1.cpp
+++ Luv/pointer1.cpp
@@ -8,6 +8,7 @@ int main(){
     cout<<"Address of the num is :"<<&num<<endl;
 
      int *ptr=&num;
-     cout<<"value of num is:"<<*ptr<<endl; // it will get the value of num
+     cout<<"value of num is:"<<*ptr<<endl; // it will get the value stored at ptr
      cout<<"value of num is :"<<num<<endl; // it will also get the value of the num
+     cout<<"address: "<<ptr<<endl; // it will show the address
 }
