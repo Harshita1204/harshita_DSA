7c532357d57c555fa0eb5f4a556e7aec59dfebd7
Harshita <harshitak1204@gmail.com>
2026-02-18 00:18:12 +0530

    stack using class

---
+++ Luv/stack.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+class Stack{
+    // properties
+    public:
+    int *arr;
+    int top;
+    int size;
+
+    // behavior
+    Stack(int size){
+        this->size=size;
+        arr=new int[size];
+        top=-1;
+    }
+    void push(int element){
+        if(size-top){
+            top++;
+            arr[top]=element;
+        }
+        else{
+            cout<<"stack overflow"<<endl;
+        }
+    }
+    void pop(){
+        if(top>=0){
+            top--;
+        }
+        else{
+            cout<<"stack underflow"<<endl;
+        }
+    }
+    int peek(){
+        if(top>=0){
+            return arr[top];
+        }else{
+            cout<<"stack is empty"<<endl;
+            return -1;
+        }
+    }
+    bool isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+int main(){
+    Stack st(5);
+
+     st.push(43);
+     st.push(34);
+     st.push(56);
+     cout<<st.peek()<<endl;
+     st.pop();
+     cout<<st.peek()<<endl;
+     st.pop();
+     cout<<st.peek()<<endl;
+     st.pop();
+     cout<<st.peek()<<endl;
+     return 0;
+}
