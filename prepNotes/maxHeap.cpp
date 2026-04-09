ac6387987fa77f82d50344ee4cc1619bbe9e79bf
Harshita <harshitak1204@gmail.com>
2026-04-09 15:25:13 +0530

    dsa

---
+++ prepNotes/maxHeap.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class maxHeap{
+    int *arr; // dynamic array
+    int size;
+    int total_size;
+
+    public:
+    maxHeap(int n){
+        arr=new int[n];
+        size=0;
+        total_size=n;
+    }
+    void insert(int val){
+        if(size== total_size){
+            cout<<"heap overflow";
+            return;
+        }
+        arr[size]=val;
+        int index=size;
+        size++;
+
+        while(index>0 && arr[(index-1)/2] <arr[index]){
+            swap(arr[(index-1)/2],arr[index]);
+            index=(index-1)/2;
+        }
+        cout<<val <<" Inserted in heap"<<endl;
+    }
+    void print(){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<endl;
+    }
+}
+};
+
+int main (){
+    maxHeap h1(10);
+    h1.insert(92);
+    h1.insert(74);
+    h1.insert(93);
+    h1.insert(49);
+    h1.insert(87);
+    h1.insert(76);
+    h1.insert(23);
+    h1.insert(33);
+
+    h1.print();
+
+}
