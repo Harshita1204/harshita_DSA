2d8715f5290f55238ec806bb0f60b7da553c7d7f
Harshita <harshitak1204@gmail.com>
2026-04-11 22:08:05 +0530

    notes

---
+++ prepNotes/preorder_traversal.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value){
+    Node* newNode=new Node();
+    newNode->data=value;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    return newNode;
+}
+
+//visit root first
+void preorder(Node* root){
+    if(root==NULL) return;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
+    Node* root=createNode(10);
+    root->left=createNode(5);
+    root->right=createNode(20);
+    preorder(root);
+}
